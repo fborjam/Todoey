dabd254ced4b91f3a28f7e6b61b623ec2f4868c5
Fernando Borja <fborja@developments-imac.local>
2018-08-01 16:19:49 -0500

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Fernando Borja on 8/1/18.
+//  Copyright © 2018 yankuiapps. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
