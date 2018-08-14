d6f47a06d5a883da33c7731c1e42b43bf00ed619
Fernando Borja <fernando@fernandos-macbook-pro.local>
2018-08-13 21:56:51 -0500

    Enable CoreData and add CategoryViewController

---
+++ Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,28 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by Fernando Borja on 8/6/18.
+//  Copyright © 2018 yankuiapps. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    //MARK: - Add New Categories
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+    }
+    
+    //MARK: - TableView Datasource Methods
+    
+    //MARK: - TableView Delegate Methods
+    
+    //MARK: - Data Manipulation Methods
+}
